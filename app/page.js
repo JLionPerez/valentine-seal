cbc15d0c69f8ab41f2c569828bab27bd5cbbea45
JLionPerez <49221382+JLionPerez@users.noreply.github.com>
2026-02-10 13:20:26 -0800

    Add text when image clicked

--- app/page.js
+++ app/page.js
@@ -6,16 +6,19 @@ export default function Home() {
   const image2 = "/test-imgs/seal2.png"
 
   const [image, setImage] = useState(image1)
-  
+  const [text, setText] = useState("")
+
   function handleClick() {
     image == image1 ? setImage(image2) : setImage(image1)
+    image == image2 ? setText("I love you seal-ots!") : setText("")
   }
 
   return (
-    <div className="flex items-center h-dvh bg-[url(/hearts.svg)] bg-rose-400 bg-center bg-repeat-round">
-
+    
+    <div className="flex items-center justify-center h-dvh bg-rose-400 text-white">
+      <p>{text}</p>
       <div onClick={handleClick}>
-        <img src={image} hidden alt="seal"></img>
+        <img src={image} alt="seal"/>
       </div>
     </div>
   )
