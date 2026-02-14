876e6cfa738ffd8ceb8818dcfccbde54247244d8
JLionPerez <49221382+JLionPerez@users.noreply.github.com>
2026-02-13 17:31:15 -0800

    Add nextjs configurations for pages

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,11 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  output: "export",
+  basePath: "/nextjs-github-pages",
+  images: {
+    unoptimized: true,
+  }
 };
 
 export default nextConfig;
